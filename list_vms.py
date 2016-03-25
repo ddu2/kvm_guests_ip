0b8ecd91b025a30839cc6e9e67fa10da3bea5ac6
du <du@dhcp-192-64.pek.redhat.com>
2016-03-25 20:36:54 +0800

    fixed some bugs.

--- list_vms.py
+++ list_vms.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-# This script will run well only if the arp cache is complete,
-# which means the items in arp table  will not be deleted if 
-# you did not touch the vm for a very long time.
-# Also, this script suppose every vm only have one IP address,
-# If you have more than one IP address on the vm, you will only
-# get one IP address.
-
-# script version:   0.2
-# python version:   2.7
+#
+# version:   0.3
+# python :   2.7
+# 
+# data: 2016-03-25
+#
 
 import subprocess
 import xml.etree.ElementTree as ET
@@ -18,64 +15,51 @@ if os.getuid() != 0:
     print "This script need to be run by user root!"
     quit()
 
-print
-print "\t======================================================="
-print "\tPlease note that this script only has been tested on RHEL6,"
-print "\tif you want to use it in other versions of RHEL or other"
-print "\tdistributions, you may need to customize it."
-print "\t======================================================="
-print
-
 # get the running vms lists and save them into a list named running_vms
 list_vms = "virsh list | grep running | awk '{print $2}'"
 running_vms = subprocess.Popen(list_vms, shell=True, stdout=subprocess.PIPE).stdout.read().strip().split()
 
-# Choose whether you want to update the arp table or not.
-print "\tDo you want to update your arp table on your kvm host (y/N)?"
-print "\tPlease note you need to install fping package to accomplish this action"
-choice = raw_input("\tYour choice:  ")
-print
-
-# use fping command to ping the network
-if choice == "Y" or choice == "y":
-    print "\tScanning local subnet 192.168.122.0/24 ............"
-    os.system('fping -c1 -g 192.168.122.0/24 > /dev/null 2>&1')
-    print
-
-# get the ip and mac map and save them into a list named arp_table
-list_arp = "arp -an | sed 's/(//g' | sed 's/)//g' | grep -v incomplete | grep 192.168.122 | awk '{print $2,$4}'"
-arp_table = subprocess.Popen(list_arp, shell=True, stdout=subprocess.PIPE).stdout.read().strip().split()
-
-# create a dictionary and save the ip/mac pairs to it.
-ip_mac_table = dict()
-while arp_table:
-    a = arp_table.pop(0)
-    ip_mac_table[a] = arp_table.pop(0)
+# get the ip and mac address map
+list_arp = "arp -an | sed 's/(//g' | sed 's/)//g' | grep -v incomplete | grep 192.168.122 | awk '{print $4,$2}'"
+def get_ip_mac_table():
+    arp_table = subprocess.Popen(list_arp, shell=True, stdout=subprocess.PIPE).stdout.read().strip().split()
+    ip_mac_table = {}
+    while arp_table:
+        mac = arp_table.pop(0)
+        ip = arp_table.pop(0)
+        if not ip_mac_table.get(mac,None):  
+            ip_mac_table[mac] = [ip]
+        else:
+            ip_mac_table[mac].append(ip)
+    return ip_mac_table
 
 # get the ip address of a vm
 def get_ip_address(node):
+    ip_list = []
     try:
         filename = '/etc/libvirt/qemu/' + node + '.xml'
         tree = ET.parse(filename)
         for mac in tree.findall('./devices/interface/mac'):
-            mac_address = mac.attrib['address']
-            try:
-                for (ip,mac_addr) in ip_mac_table.items():
-                    if mac_addr == mac_address:
-                        print ip, 
-            except:
-                print "Could not find IP Address assigned!!!"
+            if mac.attrib['address'] in ip_mac_table:
+                ip_list += ip_mac_table[mac.attrib['address']]
     except IOError as ioerror:
         print "Fire error: " + str(ioerror)
+    return ip_list
 
-print "\tCurrent running virtual machines are: "
+# refresh arp table by fping command
+def refresh_arp():
+    os.system('fping -c1 -g 192.168.122.0/24 > /dev/null 2>&1')
 
+ip_mac_table = get_ip_mac_table()
+
+print "===== Host ===\t\t\t=== IP ==="
 for vm in running_vms:
-    print "\t======",
-    print vm,
-
-    print "======:\n\t\t",
-    get_ip_address(vm)
-    print "\n"
+    print "===== " + vm,
+    ips = get_ip_address(vm)
+    if not len(ips):
+        refresh_arp()
+        ip_mac_table = get_ip_mac_table()
+        ips = get_ip_address(vm)
+    print "\t\t" + ' '.join(get_ip_address(vm))
 
 print
